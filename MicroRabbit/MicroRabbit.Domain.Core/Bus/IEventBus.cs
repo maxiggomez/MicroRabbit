6264e3ed223c21f5cfc097723d1d9eda023d8d86
Maxi Gómez <56489609+maxiggomez@users.noreply.github.com>
2020-03-14 16:04:14 -0300

    Implementación de interfaces y clases para Domain.Core Esqueleto para implementar un service bus.

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,27 @@
+﻿using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    /// <summary>
+    /// Interfaz para implementar un event Bus
+    ///     - Enviar un comando.
+    ///     - Publicar un evento.
+    ///     - Subscribirse a un evento.
+    /// </summary>
+    interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+
+        void Publish<T>(T @event) where T : Event;
+
+        void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    /// <summary>
+    /// Interfaz para implementar eventos asociados a un Bus
+    /// </summary>
+    /// <typeparam name="TEvent"></typeparam>
+    public interface IEventHandler<in TEvent> : IEventHandler
+        where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+}
